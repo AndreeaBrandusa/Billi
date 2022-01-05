3e68ae9ffaef93c796412ba525abba3f2a07e23a
AndreeaBrandusa <andreea.brandusa01@e-uvt.ro>
2022-01-05 19:42:31 +0200

    add pages (About, Account, Cart, Contact, ProductDetails) and modify Index, Privacy and Products page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,31 @@ namespace BilliWebApp.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Account()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,57 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+//-------------js for toggle menu-----------
 
-// Write your JavaScript code.
+function menutoggle() {
+    var MenuItems = document.getElementById("MenuItems");
+
+    MenuItems.style.maxHeight = "0px";
+
+    if (MenuItems.style.maxHeight == "0px") {
+        MenuItems.style.maxHeight = "200px"
+    }
+    else {
+        MenuItems.style.maxHeight = "0px"
+    }
+}
+
+//--------------js for toggle form----------->
+
+function register() {
+    var RegForm = document.getElementById("RegForm");
+    var Indicator = document.getElementById("Indicator");
+    LoginForm.style.transform = "translateX(0px)";
+    RegForm.style.transform = "translateX(0px)";
+    Indicator.style.transform = "translateX(100px)";
+}
+
+
+function login() {
+    var LoginForm = document.getElementById("LoginForm");
+    var Indicator = document.getElementById("Indicator");
+    LoginForm.style.transform = "translateX(300px)";
+    RegForm.style.transform = "translateX(300px)";
+    Indicator.style.transform = "translateX(0px)";
+}
+
+//--------------js for product gallery----------->
+
+function initProductimages() {
+    var ProductImg = document.getElementById('ProductImg')
+    var SmallImg = document.getElementsByClassName('small-img')
+
+    SmallImg[0].onclick = function () {
+        ProductImg.src = SmallImg[0].src;
+    }
+    SmallImg[1].onclick = function () {
+        ProductImg.src = SmallImg[1].src;
+    }
+    SmallImg[2].onclick = function () {
+        ProductImg.src = SmallImg[2].src;
+    }
+    SmallImg[3].onclick = function () {
+        ProductImg.src = SmallImg[3].src;
+    }
+}
 
 
 
